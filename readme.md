15f886dac98170e8e61932ce9d7cde5462052294
kaopeter <kaopeter@gmail.com>
2019-10-25 23:09:27 +0800

    sample err

--- readme.md
+++ readme.md
@@ -1,11 +1,11 @@
-#scan code
+# scan code
 ```
 docker run -d --name sonarqube -p 9000:9000 sonarqube
 mvn sonar:sonar
 
 ```
 
-#open report site
+# open report site
 ```
 http://localhost:9000/
 ```
--- src/main/java/com/codewithmansoor/GreetingResource.java
+++ src/main/java/com/codewithmansoor/GreetingResource.java
@@ -2,15 +2,20 @@ package com.codewithmansoor;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Path("/greet")
 public class GreetingResource {
+    final Logger logger = LoggerFactory.getLogger(GreetingResource.class);
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -19,7 +24,13 @@ public class GreetingResource {
     }
 
     @POST
-    public void showme(String input) {
+    public void showmeError(String input) {
+        java.nio.file.Path filePath = Paths.get("/tmp", input);
+        if (!Files.exists(filePath)) {
+            logger.warn("File does not exist", filePath.toAbsolutePath().toString());
+            return;
+        }
+        // expect to see error
         File file = new File("/Users/pankaj/source.txt" + input);
         byte[] b = new byte[100];
         try (FileInputStream fis = new FileInputStream(file)) {
@@ -27,9 +38,7 @@ public class GreetingResource {
         }
 
         catch (Exception e) {
-            // TODO: handle exception
-
-            System.err.println("Desktop is not supported");
+            logger.error("Err", e);
         }
         // first check if Desktop is supported by Platform or not
         // let's try to open PDF file
